c7f0003fc31c18cda30aa79c59f9c099ef9ba516
Yulia <yuliia.serhienko@gmail.com>
2019-05-24 20:35:02 +0300

    Celery worker added and fixed scheduled task

--- sharing/files_sharing/models.py
+++ sharing/files_sharing/models.py
@@ -1,6 +1,9 @@
-from django.contrib.auth.models import User
-from django.db import models
 import datetime
+
+from django.db import models
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
+from django.contrib.auth.models import User
 
 
 class File(models.Model):
@@ -19,3 +22,7 @@ class File(models.Model):
         self.file_lifetime_date = self.created + datetime.timedelta(days=self.file_lifetime)
         super().save(*args, **kwargs)
 
+
+@receiver(post_delete, sender=File)
+def delete_file_from_server(sender, instance, **kwargs):
+    instance.file.delete(False)
